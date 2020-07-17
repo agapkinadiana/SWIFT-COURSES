740539cedcf70cc663b82cff2508393c5d647928
agapkinadiana <agapkinadiana@mail.ru>
2020-07-17 03:21:10 +0300

    done

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Diana Agapkina on 7/9/20.
+//  Copyright © 2020 Diana Agapkina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
