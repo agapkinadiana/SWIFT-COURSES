c60a9fc7b4a895136e68f5ffae525ce4e7711406
agapkinadiana <agapkinadiana@mail.ru>
2020-07-27 20:06:26 +0300

    done

--- ClassesDemo/ClassesDemo/main.swift
+++ ClassesDemo/ClassesDemo/main.swift
@@ -1,2 +1,11 @@
 
 let skeleton = Enemy()
+skeleton.move()
+skeleton.attack()
+
+let dragon = Dragon()
+dragon.wingSpan = 5
+dragon.attackStrenght = 15
+dragon.talk(speech: "My teeth are swords! My claws are spears! My wings are a hurricane")
+dragon.move()
+dragon.attack()
