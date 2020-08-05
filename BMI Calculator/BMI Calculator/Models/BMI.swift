11181d2b63fff0dace05d2f8fa275d85445f1f36
agapkinadiana <agapkinadiana@mail.ru>
2020-08-05 18:21:18 +0300

    completed

---
+++ BMI Calculator/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Diana Agapkina on 8/5/20.
+//  Copyright © 2020 Diana Agapkina. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
