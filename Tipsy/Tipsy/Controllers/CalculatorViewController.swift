46820615e45c2a57c5de6fcaae7db98745207b02
agapkinadiana <agapkinadiana@mail.ru>
2020-08-06 21:26:37 +0300

    Step 4 Solution

--- Tipsy/Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Tipsy/Controllers/CalculatorViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.10
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+ 
+    @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        let buttonTitle = sender.currentTitle!
+        
+        //Remove the last character (%) from the title then turn it back into a String.
+        let buttonTitleMinusPercentSign = String(buttonTitle.dropLast())
+        
+        //Turn the String into a Double.
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+        
+        tip = buttonTitleAsANumber / 100
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        print(tip)
+    }
 }
 
