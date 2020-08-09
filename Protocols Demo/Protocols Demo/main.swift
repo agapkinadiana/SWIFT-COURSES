934b883a4b177e32622937b811b330bb647273c8
agapkinadiana <agapkinadiana@mail.ru>
2020-08-09 23:26:38 +0300

    about protocols

---
+++ Protocols Demo/Protocols Demo/main.swift
@@ -0,0 +1,65 @@
+
+protocol CanFly {
+    func fly()
+}
+
+class Bird {
+    
+    var isFemale = true
+    
+    func layEgg() {
+        if isFemale {
+            print("The bird makes a new bird in a shell")
+        }
+    }
+    
+//    func fly() {
+//        print("The bird flaps its wings and lifts off into the sky")
+//    }
+}
+
+class Eagle: Bird, CanFly {
+    
+    func fly() {
+        print("The eagle flaps its wings and lifts off into the sky")
+    }
+    
+    func soar() {
+        print("The eagle glides in the air using air currents")
+    }
+}
+
+class Penguin: Bird {
+    
+    func swim() {
+        print("The penguin paddles through the water")
+    }
+}
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: CanFly) {
+        flyingObject.fly()
+    }
+}
+
+struct Airplane: CanFly {
+    func fly() {
+        print("The airplane uses its engine to lift off into the air ")
+    }
+}
+
+let myEagle = Eagle()
+//myEagle.fly()
+//myEagle.layEgg()
+//myEagle.soar()
+
+let myPenguin = Penguin()
+//myPenguin.layEgg()
+//myPenguin.swim()
+//myPenguin.fly()       //but penguins can't actually fly :(
+
+let myPlane = Airplane()
+//myPlane.layEgg()      //makes absolutely zero sense
+
+let museum = FlyingMuseum()
+museum.flyingDemo(flyingObject: myEagle)
