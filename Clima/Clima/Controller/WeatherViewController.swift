77ec16a937c82ca429190b1473fe9da12b0162e3
agapkinadiana <agapkinadiana@mail.ru>
2020-08-09 21:23:56 +0300

    add dark mode + work with UITextField

---
+++ Clima/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Diana Agapkina on 8/7/20.
+//  Copyright © 2020 Diana Agapkina. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchTextField.delegate = self
+    }
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //Use searchTextField.text to get the weather for that city.
+        
+        searchTextField.text = ""
+    }
+    
+}
+
