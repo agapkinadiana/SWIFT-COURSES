cb95e2a697d6c9d3d11495902c7d721086544b9d
agapkinadiana <agapkinadiana@mail.ru>
2020-09-07 12:11:21 +0300

    add: layout + segues

---
+++ Flash iOS Chat/Flash iOS Chat/Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Flash iOS Chat
+//
+//  Created by Diana Agapkina on 9/7/20.
+//  Copyright © 2020 Diana Agapkina. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+    
+}
---
+++ Flash iOS Chat/Flash iOS Chat/Controller/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  Flash iOS Chat
+//
+//  Created by Diana Agapkina on 9/7/20.
+//  Copyright © 2020 Diana Agapkina. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+    }
+    
+}
