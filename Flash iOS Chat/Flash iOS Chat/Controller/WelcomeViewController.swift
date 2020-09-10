7f406f7acee6b4adce2d6e905fd2c5aef850959b
agapkinadiana <agapkinadiana@mail.ru>
2020-09-10 13:11:52 +0300

    add: constants + work with static

--- Flash iOS Chat/Flash iOS Chat/Controller/WelcomeViewController.swift
+++ Flash iOS Chat/Flash iOS Chat/Controller/WelcomeViewController.swift
@@ -16,7 +16,7 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         
         titleLabel.text = ""
-        let titleText = "⚡️FlashChat"
+        let titleText = Constants.appName
         var charIndex = 0.0
 
         for letter in titleText {
