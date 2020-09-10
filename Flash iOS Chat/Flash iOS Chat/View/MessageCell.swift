0122c80d63b5b2b1bb19faeb9d6f96b2211e13d8
agapkinadiana <agapkinadiana@mail.ru>
2020-09-10 19:39:31 +0300

    add: customising Cells in a TableView using a .xib File

---
+++ Flash iOS Chat/Flash iOS Chat/View/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  Flash iOS Chat
+//
+//  Created by Diana Agapkina on 9/10/20.
+//  Copyright © 2020 Diana Agapkina. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
