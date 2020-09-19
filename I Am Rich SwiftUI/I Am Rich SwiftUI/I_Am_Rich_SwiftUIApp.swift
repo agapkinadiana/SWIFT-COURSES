c561fdd19d4cfeabf2c8617c08854395999ea9de
Diana Agapkina <agapkinadiana@users.noreply.github.com>
2020-09-19 14:30:41 +0300

    add: first SwiftUI project

---
+++ I Am Rich SwiftUI/I Am Rich SwiftUI/I_Am_Rich_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_SwiftUIApp.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Diana Agapkina on 9/19/20.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
